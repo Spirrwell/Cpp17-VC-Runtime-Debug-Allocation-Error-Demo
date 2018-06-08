ea9e4ea10cef428a1af1c254611c578e176c060f
Spirrwell <Spirrwell@gmail.com>
2018-06-08 04:51:32 -0400

    Cleaner Formatting

--- Cpp17DebugAllocError/Cpp17DebugAllocError/memoryoverride.cpp
+++ Cpp17DebugAllocError/Cpp17DebugAllocError/memoryoverride.cpp
@@ -3,20 +3,20 @@
 void *__cdecl operator new(std::size_t size, int blockuse, const char *pszFileName, int lineNum)
 {
 	auto ptr = malloc(size);
-    std::cout << pszFileName << " : " << lineNum << " : " << "unaligned new(" << size << ") = " << ptr << '\n';
-    return ptr ? ptr : throw std::bad_alloc{};
+	std::cout << pszFileName << " : " << lineNum << " : " << "unaligned new(" << size << ") = " << ptr << '\n';
+	return ptr ? ptr : throw std::bad_alloc{};
 }
 
 void __cdecl operator delete(void *ptr, std::size_t size)
 {
 	std::cout << "unaligned sized delete(" << ptr << ", " << size << ")\n";
-    free(ptr);
+	free(ptr);
 }
 
 void __cdecl operator delete(void *ptr)
 {
 	std::cout << "unaligned unsized delete(" << ptr << ")\n";
-    free(ptr);
+	free(ptr);
 }
 
 void __cdecl operator delete(void *ptr, int blockuse, const char *pszFileName, int lineNum)
@@ -27,20 +27,17 @@ void __cdecl operator delete(void *ptr, int blockuse, const char *pszFileName, i
 
 // "new" over-aligned overloads
 void *__cdecl operator new(std::size_t size, std::align_val_t align) {
-    auto ptr = _aligned_malloc(size, static_cast<std::size_t>(align));
-    std::cout << "aligned new(" << size << ", " <<
-        static_cast<std::size_t>(align) << ") = " << ptr << '\n';
-    return ptr ? ptr : throw std::bad_alloc{};
+	auto ptr = _aligned_malloc(size, static_cast<std::size_t>(align));
+	std::cout << "aligned new(" << size << ", " << static_cast<std::size_t>(align) << ") = " << ptr << '\n';
+	return ptr ? ptr : throw std::bad_alloc{};
 }
 
 void __cdecl operator delete(void* ptr, std::size_t size, std::align_val_t align) {
-    std::cout << "aligned sized delete(" << ptr << ", " << size << 
-        ", " << static_cast<std::size_t>(align) << ")\n";
-    _aligned_free(ptr);
+	std::cout << "aligned sized delete(" << ptr << ", " << size << ", " << static_cast<std::size_t>(align) << ")\n";
+	_aligned_free(ptr);
 }
 
 void __cdecl operator delete(void* ptr, std::align_val_t align) {
-    std::cout << "aligned unsized delete(" << ptr << 
-        ", " << static_cast<std::size_t>(align) << ")\n";
-    _aligned_free(ptr);
+	std::cout << "aligned unsized delete(" << ptr << ", " << static_cast<std::size_t>(align) << ")\n";
+	_aligned_free(ptr);
 }
